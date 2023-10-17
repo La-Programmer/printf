4be974d118b20e30fea21e066cc2ea5ee29fd4d1
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-10-17 07:40:44 +0000

    Pushing test file

--- test_dir/0-main.c
+++ test_dir/0-main.c
@@ -8,9 +8,11 @@ int main(void)
 {
 	char c = 97;
 	char *str = "String output case";
+	char *str2 = "smart";
 	int a, b;
 
-	_printf("Obi is %c boy\n", c);
+	/*_printf("Obi is %c boy\n", c, str); It printed, it shouldn't*/
+	/*_printf("Obi is %c %s boy\n", str2, c); Supposed to give an error*/
 	a = _printf("%s\n", str);
 	_printf("Print edge case: %%\n");
 	_putchar('\n');
